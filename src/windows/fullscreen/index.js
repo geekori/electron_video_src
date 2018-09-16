fdcb587970dd4e1eeffa4650f5627949d423adba
lining <helloworld@126.com>
2018-09-16 21:46:16 +0800

    noborder

--- src/windows/fullscreen/index.js
+++ src/windows/fullscreen/index.js
@@ -6,13 +6,18 @@ fullscreen:true
 
 maxWidth、maxHeight
 
+fullscreenable:false，在Mac OS X会阻止单击最大化按钮隐藏菜单
 
+setFullScreen方法可以动态将窗口设置为全屏状态，但fullscreenable属性值必须是true
+
+通过win.isFullScreen()方法可以获取窗口是否为全屏
  */
 const {app,BrowserWindow} = require('electron');
 function createWindow() {
-    win = new BrowserWindow({fullscreen:true,maxWidth:600,maxHeight:400});
+    win = new BrowserWindow({width:400,height:400});
     win.loadFile('index.html');
-
+    win.setFullScreen(true);
+    win.isFullScreen();
     win.on('closed',()=> {
         console.log('closed');
         win = null;
---
+++ src/windows/noborder/index.js
@@ -0,0 +1,28 @@
+// 无边框窗口和透明窗口
+
+const {app,BrowserWindow} = require('electron');
+function createWindow() {
+    win = new BrowserWindow({frame:false,transparent:true});
+    win.loadFile('index.html');
+
+    win.on('closed',()=> {
+        console.log('closed');
+        win = null;
+    });
+}
+
+app.on('ready',createWindow);
+app.on('window-all-closed',()=>{
+    console.log('window-all-closed');
+    if(process.platform != 'darwin') {
+      app.quit();
+    }
+});
+
+app.on('activate',() =>{
+    console.log('activate');
+    if(win == null) {
+        createWindow();
+    }
+})
+
