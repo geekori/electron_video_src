b765c3aad3c5611c796c6bebaae84e0dd3c5e536
lining <helloworld@126.com>
2018-10-03 16:16:06 +0800

    open2

--- src/windows/window_open/event.js
+++ src/windows/window_open/event.js
@@ -13,9 +13,47 @@ window.open方法的返回值
 
 BrowserWindowProxy：可以认为是BrowserWindow的代理类
 
- */
+控制窗口
 
+1. 获取窗口焦点：focus
+2. 让窗口失去焦点状态：blur
+3. 关闭窗口：close
+4. 显示打印对话框：print
+ */
+//  打开一个子窗口
 function onClick_OpenWindow() {
     win = window.open('./child.html','子窗口','width=300,height=200');
    // win = window.open('https://geekori.com')
+}
+
+//  获取窗口焦点
+function onClick_Focus() {
+    if(win != undefined) {
+        win.focus();
+    }
+}
+
+//  让窗口失去焦点
+function onClick_Blur() {
+    if(win != undefined) {
+        win.blur();
+    }
+}
+
+//  关闭窗口
+function onClick_Close() {
+    if(win != undefined) {
+        if(win.closed) {
+            alert('子窗口已经关闭，不需要再关闭');
+            return;
+        }
+        win.close();
+    }
+}
+
+//  显示打印对话框
+function onClick_PrintDialog() {
+    if(win != undefined) {
+        win.print();
+    }
 }
