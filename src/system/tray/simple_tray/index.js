5aff7497e496f17585f4f16d626612de3a0ef47c
lining <helloworld@126.com>
2018-10-04 13:15:28 +0800

    tray

--- src/system/tray/simple_tray/index.js
+++ src/system/tray/simple_tray/index.js
@@ -1,11 +1,13 @@
 /*
-托盘应用
+为应用程序添加多个托盘图标
 
 
  */
 const {app,Menu,Tray,BrowserWindow} = require('electron');
 
 let tray;
+let tray1;
+let tray2;
 let contextMenu;
 function createWindow() {
     win = new BrowserWindow({width:800,height:600});
@@ -19,9 +21,22 @@ function createWindow() {
         {label:'粘贴',role:'paste'},
         {label:'剪切',role:'cut'}
     ])
+
+
     tray.setToolTip('这是一个托盘应用');
     tray.setContextMenu(contextMenu);
 
+    tray1 = new Tray('../../../../images/note1.png');
+    tray1.setToolTip('This is a note');
+
+    tray2 = new Tray('../../../../images/abc.png');
+
+    contextMenu1 = Menu.buildFromTemplate([
+        {label:'关闭',role:'close'},
+
+    ])
+
+    tray2.setContextMenu(contextMenu1);
 
     win.on('closed',()=> {
         console.log('closed');
