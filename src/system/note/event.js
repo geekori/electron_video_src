bf450aa9ef4caad129533130c06637a8e56f103b
lining <helloworld@126.com>
2018-10-14 18:15:43 +0800

    note

---
+++ src/system/note/event.js
@@ -0,0 +1,31 @@
+//  数据存储：用localStorage读写键值数据
+
+//  Web和Electron的数据存储方案：前端和后端
+
+/*
+键值存储：localStorage、IndexedDB
+PouchDB：客户端库
+
+SQLite
+
+
+键值：key-value，相当于Map或Dict
+ */
+
+const electron = require('electron');
+const app = electron.remote.app;
+
+function initialize() {
+    let notes = window.localStorage.notes;
+    if(!notes) notes = '记录生活的点点滴滴...';
+    textarea.value = notes;
+}
+function saveNotes() {
+    let notes = textarea.value;
+    window.localStorage.setItem('notes',notes);
+}
+
+function quit() {
+    app.quit();
+}
+window.onload = initialize;
