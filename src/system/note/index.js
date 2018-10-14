befe9b6f90cf9366fbb57f9f0c16d486c6ad8e11
lining <helloworld@126.com>
2018-10-14 18:16:35 +0800

    note1

---
+++ src/system/note/index.js
@@ -0,0 +1,20 @@
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 480,
+    height: 320,
+    frame: false
+  });
+  mainWindow.loadFile('./index.html');
+  mainWindow.on('closed', () => { mainWindow = null; });
+});
---
+++ src/system/note/run.js
@@ -0,0 +1,4 @@
+var exec = require('child_process').exec;
+free = exec('electron . ');
+
+  
